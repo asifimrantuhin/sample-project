88c6275a25e57bd905196ce798b3fbbec0298917
Asif Imran <asif.imran@ssgbd.com>
2022-09-26 23:53:55 +0600

    update

--- index.php
+++ index.php
@@ -1,15 +1,42 @@
 <?php 
 include "inc/header.php";
 ?>
+<script src="https://www.gstatic.com/charts/loader.js"></script>
+<script>
+  google.charts.load('current', {packages: ['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
 
+  function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Status', 'Amount'],
+          ['Profit',     111],
+          ['Loss',      22]
+        ]);
+
+        var options = {
+          title: 'Profit Loss Status'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+
+
+
+
+
+      }
+
+</script>
  <div class="main-panel">
         <div class="content-wrapper">
           <div class="row">
             <div class="col-md-12 grid-margin">
               <div class="row">
                 <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                  <h3 class="font-weight-bold">Welcome Aamir</h3>
-                  <h6 class="font-weight-normal mb-0">All systems are running smoothly! You have <span class="text-primary">3 unread alerts!</span></h6>
+                  <h3 class="font-weight-bold">Welcome to Dashboard</h3>
+                  
                 </div>
                 <div class="col-12 col-xl-4">
                  <div class="justify-content-end d-flex">
@@ -29,7 +56,7 @@ include "inc/header.php";
               </div>
             </div>
           </div>
-          <div class="row">
+          <!-- <div class="row">
             <div class="col-md-6 grid-margin stretch-card">
               <div class="card tale-bg">
                 <div class="card-people mt-auto">
@@ -90,32 +117,25 @@ include "inc/header.php";
                 </div>
               </div>
             </div>
-          </div>
+          </div> -->
           <div class="row">
             <div class="col-md-6 grid-margin stretch-card">
               <div class="card">
                 <div class="card-body">
-                  <p class="card-title">Order Details</p>
-                  <p class="font-weight-500">The total number of sessions within the date range. It is the period time a user is actively engaged with your website, page or app, etc</p>
-                  <div class="d-flex flex-wrap mb-5">
-                    <div class="mr-5 mt-3">
-                      <p class="text-muted">Order value</p>
+                  <p class="card-title">Order History</p>
+                  
+                  <div class="row mb-5">
+                    <div class="col-lg-6 col-md-6 col-sm-6 mt-3">
+                      <p class="text-muted">Profit</p>
                       <h3 class="text-primary fs-30 font-weight-medium">12.3k</h3>
                     </div>
-                    <div class="mr-5 mt-3">
-                      <p class="text-muted">Orders</p>
+                    <div class="col-lg-6 col-md-6 col-sm-6 mt-3">
+                      <p class="text-muted">Loss</p>
                       <h3 class="text-primary fs-30 font-weight-medium">14k</h3>
                     </div>
-                    <div class="mr-5 mt-3">
-                      <p class="text-muted">Users</p>
-                      <h3 class="text-primary fs-30 font-weight-medium">71.56%</h3>
-                    </div>
-                    <div class="mt-3">
-                      <p class="text-muted">Downloads</p>
-                      <h3 class="text-primary fs-30 font-weight-medium">34040</h3>
-                    </div> 
+                     
                   </div>
-                  <canvas id="order-chart"></canvas>
+                  <div id="piechart" style="width: 100%x; height: 400px;"></div>
                 </div>
               </div>
             </div>

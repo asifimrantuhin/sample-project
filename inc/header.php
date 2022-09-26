4b1bfe519ad4e7b5c26d887dda24a3fb85aff5aa
Asif Imran <asif.imran@ssgbd.com>
2022-09-26 21:29:40 +0600

    update

--- inc/header.php
+++ inc/header.php
@@ -152,7 +152,7 @@
                 <nav class="sidebar sidebar-offcanvas" id="sidebar">
                     <ul class="nav">
                         <li class="nav-item">
-                            <a class="nav-link" href="dashboard.php">
+                            <a class="nav-link" href="index.php">
                             <i class="icon-grid menu-icon"></i>
                             <span class="menu-title">Dashboard</span>
                             </a>
